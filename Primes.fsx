282924b5aab0e2509cba7f28d1d161c365ee4711
kam1986 <kam@live.dk>
2018-05-28 22:02:12 +0200

    primes

---
+++ Primes.fsx
@@ -0,0 +1,18 @@
+
+// squqreroot of a integer
+let intsqrt n = (int << sqrt << float) n
+
+let rec IsPrime p = function
+    | []                           -> true
+    | x :: _  when x > (intsqrt p) -> true
+    | x :: xs when p % x <> 0      -> IsPrime p xs
+    | _                            -> false
+
+let NthPrimes n = 
+    List.rev 
+        (List.fold 
+            (fun acc p -> 
+                if IsPrime p (List.rev acc) then 
+                    p::acc 
+                else acc)
+        [] (2::[for i in 1 .. n -> 2 * i + 1]))
